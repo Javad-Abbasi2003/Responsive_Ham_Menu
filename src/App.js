35915cf56e2be039ecc898a609cbf597f36f40c6
Javad Abbasi <eros.javad@gmail.com>
2022-08-12 01:36:11 +0430

    added desktop warning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+
 import HamburgerNav from "./HamburgerNav";
 
+import styles from "./App.module.css";
+
 function App() {
-  return <HamburgerNav />;
+  return (
+    <>
+      <HamburgerNav />
+      <h1 className={styles.header}>Please open with mobile devices.</h1>
+    </>
+  );
 }
 
 export default App;
